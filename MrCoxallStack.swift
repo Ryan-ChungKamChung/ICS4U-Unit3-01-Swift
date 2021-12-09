f48caf16f0814577d8361255d9af0ded7b5df94b
Ubuntu <ubuntu@ip-172-31-92-172.ec2.internal>
2021-12-09 17:04:04 +0000

    fixed comments

--- MrCoxallStack.swift
+++ MrCoxallStack.swift
@@ -1,10 +1,24 @@
+//
+//  MrCoxallStack.swift
+//
+//  Created by Ryan Chung
+//  Created on 2021-12-09
+//  Version 1.0
+//  Copyright (c) 2021 Ryan Chung. All rights reserved.
+//
+//  This is the stack class.
+//
+
+// Class that emulates a stack
 class MrCoxallStack {
     private var stackAsArray = [Int]()
 
+    // Pushes a number to the stack
     func push(pushNumber: Int) {
         stackAsArray.insert(pushNumber, at: 0)
     }
 
+    // Displays the stack
     func showStack() {
         for element in stackAsArray {
             print("\(element)")
--- main.swift
+++ main.swift
@@ -1,3 +1,15 @@
+//
+//  TowersOfHanoi.swift
+//
+//  Created by Ryan Chung
+//  Created on 2021-12-09
+//  Version 1.0
+//  Copyright (c) 2021 Ryan Chung. All rights reserved.
+//
+//  This program demonstrates a stack.
+//
+
+// Example of how a stack works with OOP.
 let aStack: MrCoxallStack = MrCoxallStack()
 let number1 = 7
 let number2 = 42
